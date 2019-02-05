4a87ceda6da712e25691ef7adeb16484cf1cbc40
Peters <sgjpete2@liverpool.ac.uk>
2019-02-05 12:20:03 +0000

    Histogramssss

---
+++ Task2/src/uk/ac/liverpool/sgjpete2/GenerateHistograms.java
@@ -0,0 +1,38 @@
+package uk.ac.liverpool.sgjpete2;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.Random;
+
+class GenerateHistograms
+{
+    static Random randGen = new Random();
+    static Scanner keyboard = new Scanner(System.in);
+    
+    private static double addTwelve()
+    {
+	// add 12 uniformly-distributed random numbers
+	double sum = 0.;
+	for (int n = 0 ; n < 12; n++) {
+	    sum = sum + randGen.nextDouble();
+	}
+	return (sum);
+    }
+    
+    public static void main (String [] args ) throws IOException
+    {       
+	// create an instance of the Class Histogram: 50 bins from 0.0 to 1.0
+	// this is for the flat distribution
+	Histogram hist = new Histogram(50, 0.3, 0.8, "Uniform");
+
+	System.out.println( "Input the number of random numbers to generate");
+	int trials = keyboard.nextInt();
+	for (int i = 0; i < trials; i++) {
+	    double value = randGen.nextDouble();
+	    hist.fill(value);
+	}
+
+	hist.print();
+	hist.writeToDisk("test.csv");
+    }
+}
---
+++ Task2/src/uk/ac/liverpool/sgjpete2/Histogram.java
@@ -0,0 +1,98 @@
+package uk.ac.liverpool.sgjpete2;
+
+import java.io.*;
+
+class Histogram
+{	
+    private double binlow, binhigh;
+    private double binwidth;
+    private int nbins;
+    private double[] binCentre;
+    private String histname;
+    
+    private int underflows;
+    private int overflows;
+
+    // double array to store the actual histogram data
+    private double[] sumWeights;
+
+    // constructor for the class Histogram
+    public Histogram(int numberOfBins, double start, double end, String name)
+    {
+	// store the parameters and setup the histogram
+	// note that parameters need to have different names than class variables
+	nbins = numberOfBins;
+	binlow = start;
+	binhigh = end;
+	histname = name;
+
+	binwidth = (binhigh - binlow) / (double) nbins;
+	sumWeights = new double[nbins];
+	
+	// calculate and save the x coordinate of the centre of each bin
+	binCentre = new double[nbins];
+	for (int i = 0; i < nbins; i++) {
+	    binCentre[i] = binlow + (i+0.5)*binwidth;
+	}
+    }
+
+    public int getNbins()
+    {
+	return nbins;
+    }
+
+    public void fill(double value){
+    	// fid correct bin and add 1.
+    	int ibin = (int) ( (value - binlow)/binwidth);
+	
+		if((ibin >= 0)&&(ibin < nbins)){
+			sumWeights[ibin] = sumWeights[ibin] + 1.0;
+		} 
+		else if(ibin < 0){ underflows++; }
+		else if(ibin >= nbins){ overflows++; }
+    }
+
+    public double getContent(int nbin) {
+		// returns the contents on bin 'nbin' to the user
+		return sumWeights[nbin];
+    }
+    
+    public double getError(int nbin){
+    	//Returns statistical error on  particular value.
+    	return Math.sqrt(sumWeights[nbin]);
+    }
+
+    public void print() {
+		System.out.println("Histogram " + histname);
+		for (int bin = 0; bin < getNbins(); bin++) {
+		    System.out.println("Bin " + bin + " = " +getContent(bin)+" +/- "+getError(bin));
+		}
+		
+		System.out.println("Overflows: "+overflows);
+		System.out.println("Underflows: "+underflows);
+    }
+    
+    //-------------------------------------
+    public void writeToDisk(String filename) throws IOException
+    {
+        FileWriter file = new FileWriter(filename);     // this creates the file with the given name
+        PrintWriter outputFile = new PrintWriter(file); // this sends the output to file1
+        
+
+        // Write the file as a comma seperated file (.csv) so it can be read it into EXCEL
+        // first some general information about the histogram
+        outputFile.println("histname, " + histname);
+        outputFile.println("binlow, " + binlow);
+        outputFile.println("binwidth, " + binwidth);
+        outputFile.println("nbins, " + nbins);
+
+        // now make a loop to write the contents of each bin to disk, one number at a time
+        // together with the x-coordinate of the centre of each bin.
+        for (int n = 0; n < nbins; n++) {
+            // comma separated values
+            outputFile.println(n + "," + binCentre[n] + "," + getContent(n) + "," + getError(n));
+        }
+        outputFile.close(); // close the output file
+        return;
+    }
+}
